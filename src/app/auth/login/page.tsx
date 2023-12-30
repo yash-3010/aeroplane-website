6a185bbc469a40c90c4d869fbbac922a863f27c7
Yash <yash.barman@kreeti.com>
2023-12-30 05:33:42 +0530

    third auth commit

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,6 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import Login from "@/app/components/Login";
-import Test from "@/app/components/Test";
 
 export default async function getServerSideProps() {
   
